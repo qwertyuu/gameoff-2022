01ea918bf8fb58b470c29aa74ad3fd8c23b80442
Raphaël Côté <cotlarrc@msn.com>
2022-11-16 00:37:51 -0500

    detect a main subject in photos

--- Assets/Game/Pictures/PhotoCamera.cs
+++ Assets/Game/Pictures/PhotoCamera.cs
@@ -36,21 +36,58 @@ namespace Game.Pictures
         public Picture CreatePicture(Texture photo)
         {
             Picture picture = new Picture(photo);
-
-            // RAPH: This is a sample detection script. This is meant to be better
             var planes = GeometryUtility.CalculateFrustumPlanes(_cameraLens);
             var pictureInterests = Object.FindObjectsOfType<PictureInterest>();
-            foreach (var pictureInterest in pictureInterests)
+            var elementsInPicture = GetPictureInterests(planes, pictureInterests);
+            float mainSubjectScore = float.MaxValue;
+
+            foreach (var elementInPicture in elementsInPicture)
             {
-                if (GeometryUtility.TestPlanesAABB(planes, pictureInterest.pictureInterestCollider.bounds))
+                Bounds bounds = elementInPicture.pictureInterestCollider.bounds;
+
+                Vector3 boundsCenterToScreen = _cameraLens.WorldToScreenPoint(bounds.center);
+                Vector2 boundsCenterToScreen2d = new Vector2(boundsCenterToScreen.x, boundsCenterToScreen.y);
+                Vector3 cameraElementDistance = bounds.center - _cameraLens.gameObject.transform.position;
+                Rect cameraRect = _cameraLens.pixelRect;
+                Vector2 pixelCameraCenter = new Vector2(cameraRect.width / 2f, cameraRect.height / 2f);
+                Vector2 distanceFromCenter = pixelCameraCenter - boundsCenterToScreen2d;
+                // dCenter is a diff between the center of the bounding box of the PictureInterest, translated to screen point and the center of the image (w/2, h/2)
+                var dCenter = distanceFromCenter.magnitude;
+                // dCamera is the world distance between the camera and the PictureInterest
+                var dCamera = cameraElementDistance.magnitude;
+                Debug.LogFormat("DCentre: {0} DCamera: {1} Nom: {2}", dCenter, dCamera, elementInPicture.name);
+
+                // Main subject is lowest on all axis (dCenter and dCamera)
+                float score = (new Vector2(dCenter, dCamera)).sqrMagnitude;
+                if (score < mainSubjectScore)
                 {
-                    Debug.Log(pictureInterest.name + " has been detected!");
+                    mainSubjectScore = score;
+                    // Hack here. SetMainSubject might be called many times, only the actual main subject will be set last.
+                    picture.SetMainSubject(dCenter, dCamera, elementInPicture);
                 }
+            }
+            if (picture.MainSubject)
+            {
+                Debug.LogFormat("MainSubject is {0}", picture.MainSubject.name);
             }
 
             //TODO: Create a Picture, give it the texture, find on-screen CameraSubjects, make calculations (screen space ratio, other special stuff, etc)
 
             return picture;
+        }
+
+        private PictureInterest[] GetPictureInterests(Plane[] planes, PictureInterest[] pictureInterests)
+        {
+            List<PictureInterest> elementsInPicture = new List<PictureInterest>();
+
+            foreach (var pictureInterest in pictureInterests)
+            {
+                if (GeometryUtility.TestPlanesAABB(planes, pictureInterest.pictureInterestCollider.bounds))
+                {
+                    elementsInPicture.Add(pictureInterest);
+                }
+            }
+            return elementsInPicture.ToArray();
         }
 
         public void Reinitialize()
--- Assets/Game/Pictures/Picture.cs
+++ Assets/Game/Pictures/Picture.cs
@@ -8,9 +8,25 @@ namespace Game.Pictures
         [PublicAPI]
         public Texture Texture { get; }
 
+        [PublicAPI]
+        public float MainSubjectDistanceFromCenterOfPicture { get; private set; }
+
+        [PublicAPI]
+        public float MainSubjectDistanceFromCamera { get; private set; }
+
+        [PublicAPI]
+        public PictureInterest MainSubject { get; private set; }
+
         public Picture(Texture texture)
         {
             Texture = texture;
+        }
+
+        public void SetMainSubject(float dCenter, float dCamera, PictureInterest mainSubject)
+        {
+            MainSubjectDistanceFromCenterOfPicture = dCenter;
+            MainSubjectDistanceFromCamera = dCamera;
+            MainSubject = mainSubject;
         }
     }
 }
