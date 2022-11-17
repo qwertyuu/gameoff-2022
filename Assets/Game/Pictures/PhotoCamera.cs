feb75d6fddc17bfe98939196dfe991cc20e2f897
Raphaël Côté <cotlarrc@msn.com>
2022-11-16 23:10:57 -0500

    refactor picture into multiple classes
    add looking angle to the mix

--- Assets/Game/Pictures/PhotoCamera.cs
+++ Assets/Game/Pictures/PhotoCamera.cs
@@ -47,7 +47,7 @@ namespace Game.Pictures
 
                 Vector3 boundsCenterToScreen = _cameraLens.WorldToScreenPoint(bounds.center);
                 Vector2 boundsCenterToScreen2d = new Vector2(boundsCenterToScreen.x, boundsCenterToScreen.y);
-                Vector3 cameraElementDistance = bounds.center - _cameraLens.gameObject.transform.position;
+                Vector3 cameraElementDistance = bounds.center - _cameraLens.transform.position;
                 Rect cameraRect = _cameraLens.pixelRect;
                 Vector2 pixelCameraCenter = new Vector2(cameraRect.width / 2f, cameraRect.height / 2f);
                 Vector2 distanceFromCenter = pixelCameraCenter - boundsCenterToScreen2d;
@@ -55,7 +55,13 @@ namespace Game.Pictures
                 var dCenter = distanceFromCenter.magnitude;
                 // dCamera is the world distance between the camera and the PictureInterest
                 var dCamera = cameraElementDistance.magnitude;
-                Debug.LogFormat("DCentre: {0} DCamera: {1} Nom: {2}", dCenter, dCamera, elementInPicture.name);
+                var whereElementIsHeaded = elementInPicture.transform.forward;
+                var elementToCamera = _cameraLens.transform.position - elementInPicture.transform.position;
+                // angle (0 to 180) between where the element is looking and where it should look for it to look straight at the camera
+                var hAngle = Vector3.Angle(whereElementIsHeaded, elementToCamera);
+
+                var pSubject = new PictureSubject(dCenter, dCamera, hAngle, elementInPicture);
+                picture.AddSubject(pSubject);
 
                 // Main subject is lowest on all axis (dCenter and dCamera)
                 float score = (new Vector2(dCenter, dCamera)).sqrMagnitude;
@@ -63,12 +69,13 @@ namespace Game.Pictures
                 {
                     mainSubjectScore = score;
                     // Hack here. SetMainSubject might be called many times, only the actual main subject will be set last.
-                    picture.SetMainSubject(dCenter, dCamera, elementInPicture);
+                    picture.SetMainSubject(pSubject);
                 }
             }
-            if (picture.MainSubject)
+            Debug.LogFormat("Photo contains {0} subjects", picture.Subjects.Count);
+            if (picture.MainSubject != null)
             {
-                Debug.LogFormat("MainSubject is {0}", picture.MainSubject.name);
+                Debug.LogFormat("MainSubject is {0}", picture.MainSubject);
             }
 
             //TODO: Create a Picture, give it the texture, find on-screen CameraSubjects, make calculations (screen space ratio, other special stuff, etc)
--- Assets/Game/Pictures/Picture.cs
+++ Assets/Game/Pictures/Picture.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 
@@ -9,24 +10,68 @@ namespace Game.Pictures
         public Texture Texture { get; }
 
         [PublicAPI]
-        public float MainSubjectDistanceFromCenterOfPicture { get; private set; }
+        public PictureSubject MainSubject { get; private set; }
 
         [PublicAPI]
-        public float MainSubjectDistanceFromCamera { get; private set; }
-
-        [PublicAPI]
-        public PictureInterest MainSubject { get; private set; }
+        public List<PictureSubject> Subjects { get; private set; }
 
         public Picture(Texture texture)
         {
             Texture = texture;
+            Subjects = new List<PictureSubject>();
         }
 
-        public void SetMainSubject(float dCenter, float dCamera, PictureInterest mainSubject)
+        public void SetMainSubject(PictureSubject mainSubject)
         {
-            MainSubjectDistanceFromCenterOfPicture = dCenter;
-            MainSubjectDistanceFromCamera = dCamera;
             MainSubject = mainSubject;
+        }
+
+        public void AddSubject(PictureSubject subject)
+        {
+            Subjects.Add(subject);
+        }
+    }
+
+    public class PictureSubject
+    {
+        /*
+        DistanceFromCenterOfPicture is a diff between the center of the bounding box of the PictureInterest, translated to screen point and the center of the image (w/2, h/2)
+        0 means the center of the PictureInterest is straight in the center of the image
+        otherwise it is the distance offset with the center
+        */
+        [PublicAPI]
+        public float DistanceFromCenterOfPicture { get; private set; }
+
+        /*
+        DistanceFromCamera is the world distance between the camera and the PictureInterest
+        */
+        [PublicAPI]
+        public float DistanceFromCamera { get; private set; }
+
+        /*
+        HeadingAngle (0 to 180) between where the element is looking and where it should look for it to look straight at the camera
+        0 means the PictureInterest is looking straight at the camera, 180 means looking straight away (turned around)
+        */
+        [PublicAPI]
+        public float HeadingAngle { get; private set; }
+
+        /*
+        PictureInterest is the interest itself. note that it is not a copy in time of the picture, so the values contained in this class cannot be calculated again later
+        */
+        [PublicAPI]
+        public PictureInterest PictureInterest { get; private set; }
+
+        public PictureSubject(float distanceFromCenterOfPicture, float distanceFromCamera, float headingAngle, PictureInterest pictureInterest)
+        {
+            DistanceFromCenterOfPicture = distanceFromCenterOfPicture;
+            DistanceFromCamera = distanceFromCamera;
+            HeadingAngle = headingAngle;
+            PictureInterest = pictureInterest;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("DCentre: {0} DCamera: {1} HAngle: {2} Nom: {3}", DistanceFromCenterOfPicture, DistanceFromCamera, HeadingAngle, PictureInterest.name);
         }
     }
 }
