7a6cd3c0cad03cc296c429d9cc3088da8ac2417e
nesrineCh <nesrine.chekou@hotmail.com>
2022-11-22 21:48:22 -0500

    First commit
    Ball following the player with a throw script

---
+++ Assets/Game/Chain/ChainBehaviourScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ChainBehaviourScript : MonoBehaviour
+{
+    public Transform player; //mon point de depart = joueur ? camera ? vue premiere personne ?
+    public float force;
+    public int numberOfLinks; //le nombre de maillons - pour plus tard
+    
+    bool readyToThrow;
+    //mettre un cooldown ?
+
+    private Rigidbody rbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        readyToThrow = true;
+        rbody.isKinematic = true;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rbody.isKinematic) {
+            transform.position = player.position;
+        }
+
+        if (Input.GetMouseButtonDown(0) && readyToThrow) //clique gauche
+        {
+            Throw();
+        }
+    }
+
+    private void Throw()
+    {
+        Debug.Log("throw");
+
+        readyToThrow = false;
+        rbody.isKinematic = false;
+        rbody.AddForce(player.forward * force, ForceMode.Impulse);
+
+
+        readyToThrow = true;
+    }
+
+
+}
