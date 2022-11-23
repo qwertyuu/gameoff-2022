5a1af7351179e53099805f651d39d5ddc26de905
VincentVBB <vvanbalberghe@mapgears.com>
2022-11-22 21:56:59 -0500

    properly recieve photoCamera

--- Assets/Sample/Gallery/DynamicScrollView.cs
+++ Assets/Sample/Gallery/DynamicScrollView.cs
@@ -14,27 +14,31 @@ public class DynamicScrollView : MonoBehaviour
     [SerializeField]
     private List<Picture> _pictures;
     
+    [SerializeField]
+    private PhotoCamera photoCamera;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        photoCamera = GameObject.FindGameObjectsWithTag("PhotoCamera")[0].GetComponent<PhotoCamera>();
+
         _pictures = new List<Picture>();
-        for (int i = 0; i<200; i++){
-            var texture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+        // for (int i = 0; i<200; i++){
+        //     var texture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
 
-            // set the pixel values
-            texture.SetPixel(0, 0, Color.black);
-            texture.SetPixel(1, 0, Color.black);
-            texture.SetPixel(0, 1, Color.black);
-            texture.SetPixel(1, 1, Color.black);
+        //     // set the pixel values
+        //     texture.SetPixel(0, 0, Color.black);
+        //     texture.SetPixel(1, 0, Color.black);
+        //     texture.SetPixel(0, 1, Color.black);
+        //     texture.SetPixel(1, 1, Color.black);
         
-            // Apply all SetPixel calls
-            texture.Apply();
+        //     // Apply all SetPixel calls
+        //     texture.Apply();
 
-            _pictures.Add(new Picture(texture));
-        }
+        //     _pictures.Add(new Picture(texture));
+        // }
 
 
 
